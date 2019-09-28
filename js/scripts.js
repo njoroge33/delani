987d39ab67c2d9422560712bf26f4d9e347cd47e
njoroge33 <johngichuhi769@gmail.com>
2019-09-28 20:32:57 +0300

    add a tooggle function to what we do paragraphs

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#img1").click(function(){
+        $("#dev").toggle();
+    });
+
+    $("#img2").click(function(){
+        $("#pro").toggle();
+    });
+
+    $("#img3").click(function(){
+        $("#des").toggle();
+    });
+});
